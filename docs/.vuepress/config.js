57f0f23e268f725e1dcba0959f7c5f62fd712d41
gona <gona@gmail.com>
2025-10-05 17:41:23 +0300

    change config.ts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: '/gonahoshyar.github.io/',
+  base: '/',// <--- must be root for user page
   title: 'My Portfolio',
   description: 'My VuePress Portfolio Site',
   themeConfig: {
